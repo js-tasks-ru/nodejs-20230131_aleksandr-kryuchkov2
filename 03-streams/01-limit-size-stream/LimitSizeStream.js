cb272ad75801a3101e682de4e5e6164520bbc1e1
Alexander Kryuchkov <agkruchkov@gmail.com>
2023-02-10 22:29:46 +0300

    add solution for task 3.1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.str = '';
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    this.str += chunk.toString();
 
+    if (Buffer.from(this.str).length < this.limit + 1) {
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
 
