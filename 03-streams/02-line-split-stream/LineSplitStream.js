441dfd664d54625d3f5fb10187fd98a87f05a16b
Alexander Kryuchkov <agkruchkov@gmail.com>
2023-02-15 12:51:58 +0300

    solution for task 3-2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,39 +4,34 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
-    this.array = [];
+    this.lastStrWithoutEol = '';
   }
 
   _transform(chunk, encoding, callback) {
-    // 1) работает, но тесты не проходит
-    // this.array = chunk.toString().split(os.EOL);
-    //
-    // if (this.array.length > 1) {
-    //   this.array.forEach((str) => this.push(`${str}${os.EOL}`));
-    // }
-    // callback();
+    const chunkStr = chunk.toString();
+    const arr = chunkStr.split(os.EOL);
 
-    // 2) не работает, ругается на ERR_MULTIPLE_CALLBACK
-    // this.array = chunk.toString().split(os.EOL);
-    //
-    // if (this.array.length > 1) {
-    //   this.array.forEach((str) => {
-    //     callback(null, `${str}${os.EOL}`);
-    //     this.pause();
-    //     this.once('drain', () => {
-    //       this.resume();
-    //     });
-    //   },
-    //   );
-    // }
+    const pushData = (array) => array.forEach((str) => this.push(str));
+
+    if (chunkStr.endsWith(os.EOL)) {
+      pushData(arr);
+      this.lastStrWithoutEol = '';
+    } else {
+      if (this.lastStrWithoutEol !== '') {
+        arr[0] = this.lastStrWithoutEol + arr[0];
+      };
+      this.lastStrWithoutEol = arr.pop();
+      pushData(arr);
+    }
+    callback();
   }
 
   _flush(callback) {
-    callback();
-    // callback(this.array[0]);
-    // if (this.array.length === 1) {
-    //   callback(null, this.array[0]);
-    // }
+    if (this.lastStrWithoutEol !== '') {
+      callback(null, this.lastStrWithoutEol);
+    } else {
+      callback();
+    }
   }
 }
 
